a734a2aadb8b1415276da7f395abab506eb4e554
Benjamin Champion <benjamin.champion@ligo.org>
2020-01-19 16:59:46 -0500

    Test commit for new branch

--- src/main.c
+++ src/main.c
@@ -1,26 +1,6 @@
-// Main file for the Fossil interpreter
-//
-// Ben Champion <bwc3252@rit.edu>
-
 #include <stdio.h>
 
-#include "lexer/lexer.h"
-#include "linked_list/list.h"
-#include "io/read_file.h"
-#include "parser/parser.h"
-#include "context/context.h"
-#include "interpreter/interpreter.h"
-
 int main(int argc, char **argv) {
-    if (argc < 2) {
-        fprintf(stderr, "Error: no input file\n");
-        return 1;
-    }
-    char *str = read_source_file(argv[1]);
-    list_node_t node = tokenize(str);
-    list_node_t start = node;
-    ast_node_t root = parse(node);
-    destroy_list(start);
-    free(str);
-    interpret(root);
+    printf("Hello world.\n");
+    return 0;
 }
